c2bb86ba7341e83d553b3bdf6a4060d6f0af7227
Pradipa Y <merakayamsstraat@gmail.com>
2024-06-14 09:51:57 +0700

    menambahkan fungsi if else di void insert

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -25,8 +25,28 @@ public:
         ROOT = NULL; //Init ROOT to NULL
     }
     void insert(string element) {
+        Node* newNode = new Node(element, NULL, NULL);
+        newNode->info = element; //Assign Value to data field on newNode
+        newNode->leftchild = NULL; //Make leftchild of the newnode point to NULL
+        newNode->rightchild = NULL; //Make rightchild of the newnode point to NULL
 
+        Node * parent = NULL;
+        Node* currentNode = NULL;
+        search(element, parent, currentNode); //Locate Node will be the parent of the node tobe inserted
+
+        if (parent == NULL) {
+            ROOT = newNode;
+            return;
+        }
+        if (element < parent->info) {
+            parent->leftchild = newNode;
+        }
+        else if (element > parent->info) {
+            parent->rightchild = newNode;
+        }
     }
+    
+    
 };
 
 int main()
